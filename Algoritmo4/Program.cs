c731195b9cfdbd1054b09e7b8a18a8778985365e
Millena Kappaun <kappaunmillena@gmail.com>
2025-04-01 21:35:44 -0300

    exercicios

---
+++ Algoritmo4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+//variaveis
+double largura, comprimento, area, valoMetro, valorTerreno;
+//entrada
+largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+valoMetro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+//calculo
+area = largura * comprimento;
+valorTerreno = area * valoMetro;
+//saida
+Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("PRECO = " + valorTerreno.ToString("F2", CultureInfo.InvariantCulture));
